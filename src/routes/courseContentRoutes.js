b2d9ebc83ef627465123834d1ff2a179e61cfa4f
ardiansetya <ardiansetya129@gmail.com>
2025-01-10 19:10:46 +0700

    feat/crud-content-course

--- src/routes/courseContentRoutes.js
+++ src/routes/courseContentRoutes.js
@@ -4,10 +4,10 @@ const { authenticate, isTeacher } = require('../middleware/middleware');
 const { createCourseContent, getAllCourseContents, getCourseContentById, updateCourseContent, deleteCourseContent } = require("../controllers/courseContentController");
 
 
-router.post("/courses/:courseId/contents", createCourseContent);
-router.get("/courses/contents", getAllCourseContents);
-router.get("/courses/contents/:id", getCourseContentById);
-router.put("/courses/contents/:id", updateCourseContent);
-router.delete("/courses/contents/:id", deleteCourseContent);
+router.post("/course/:courseId/contents", createCourseContent);
+router.get("/course/contents", getAllCourseContents);
+router.get("/course/contents/:id", getCourseContentById);
+router.put("/course/contents/:id", updateCourseContent);
+router.delete("/course/contents/:id", deleteCourseContent);
 
 module.exports = router;
